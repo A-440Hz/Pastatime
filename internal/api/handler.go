e5b21b73e8f3c9fb303bc1a38dd3c583c2a0a284
A-440Hz <23140824+A-440Hz@users.noreply.github.com>
2024-01-12 17:24:37 -0800

    feat: implemented logic for requesting newest SFW post
    - cleaned up loop logic for polling random SFW post
    - cleaned up testcases

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -30,20 +30,31 @@ import (
 // ---/
 
 // TODO: make these constants modifiable numbers in a config setting somewhere
+// TODO: what about paramters for these configs? limits and suggested values?
+// TODO: have a user-facing README section explaining these config values clearly.
 
 var ctx = context.Background()
 
 // requestWindowSize is the number of posts requested at a time from the reddit API. 25 seems to be a max value for the r/rising sort order
+// this value cannot be < 1
 // TODO: Write a good comment note about window size and the birthday paradox (how many calls before a repeat with pool size x)
 const requestWindowSize = 25
 
 // maxWindowShifts limits the number of API requests for subsequent listings of requestWindowSize when querying reddit post listings for a valid post.
 // if requestWindowSize is 25 and maxWindowShifts is 5, a max 150 posts could be requested from the reddit API until a valid one is found.
+// TODO: should I add an upper limit for this?
 const maxWindowShifts = 5
 
-// maxRetriesPerWindow limits the number of additional attempts made to select a valid post
-// before moving on to the next pool of requestWindowSize posts
-const maxRetriesPerWindow = 4
+// maxRandSamplesPerWindow limits the number of additional attempts made when randomly selecting a valid post
+// before decrementing maxWindowShifts and calling the API to request a new listing of posts to sample from.
+const maxRandSamplesPerWindow = 4
+
+// maxPollAttempts limits the maximum attempts made to find a valid post
+// This value helps separate the paramters used when random sampling and linear sampling for posts.
+// Linearly it could take at most maxWindowShifts * requestWindowSize operations to find (or not find) a valid post.
+// One could say maxWindowShifts * requestWindowSize is close enough to maxPollAttempts, but I think it's better to have an explicit limit.
+// TODO: should I make this cap apply for random sampling too? it should be trivial
+const maxPollAttempts = 100
 
 type RequestStrategy interface {
 	get(subreddit string, sortOrder string) (*reddit.Post, error)
@@ -51,8 +62,7 @@ type RequestStrategy interface {
 	Request(subreddit string, sortOrder string, censorStrategy string) (*reddit.Post, error)
 }
 
-// RequestNewestPost prioritizes the newest reddit post from a subreddit and sort order
-// not to be confused with "sort by /new"
+// RequestNewestPost returns the most recent reddit post from a subreddit using sortOrder "new, rising, hot"
 type RequestNewestPost struct{}
 
 func (r RequestNewestPost) get(subreddit string, sortOrder string) (*reddit.Post, error) {
@@ -78,8 +88,29 @@ func (r RequestNewestPost) getSFW(subreddit string, sortOrder string) (*reddit.P
 	if err != nil {
 		return nil, fmt.Errorf("RequestNewestPost.getSFW error with http code %q: %q", resp.Status, err.Error())
 	}
-	// TODO: iterate if NSFW
-	return posts[0], nil
+	for i := 0; i < maxPollAttempts; i++ {
+		k := i % requestWindowSize
+
+		// request new posts if needed
+		if k == 0 && i > 0 {
+			posts, resp, err = reqFunction(ctx, subreddit,
+				&reddit.ListOptions{
+					Limit: requestWindowSize,
+					After: resp.After,
+				})
+			if err != nil {
+				return nil, errors.Join(fmt.Errorf("RequestNewestPost.getSFW error with http code %q: %q", resp.Status, err.Error()),
+					fmt.Errorf("RequestNewestPost.getSFW did not find a valid post within maxPollAttempts %d", maxPollAttempts))
+			}
+		}
+
+		// return valid post immediately
+		if !goaway.IsProfane(posts[k].Title) && !goaway.IsProfane(posts[k].Body) {
+			return posts[i], nil
+		}
+
+	}
+	return nil, fmt.Errorf("RequestNewestPost.getSFW did not find a valid post within maxPollAttempts %d", maxPollAttempts)
 }
 
 // Request specifies whether the SFW filter function is called to request posts.
@@ -90,7 +121,7 @@ func (r RequestNewestPost) Request(subreddit string, sortOrder string, censorStr
 	return r.get(subreddit, sortOrder)
 }
 
-// RequestRandomPost gets a random post from the top requestWindowSize posts on the reddit "trending" filter
+// RequestRandomPost picks a random post frum subreddit using sortOrder "new, rising, or hot"
 type RequestRandomPost struct{}
 
 func (r RequestRandomPost) get(subreddit string, sortOrder string) (*reddit.Post, error) {
@@ -113,7 +144,8 @@ func (r RequestRandomPost) get(subreddit string, sortOrder string) (*reddit.Post
 	return posts[i], nil
 }
 
-// getSFW increments startPoolSize to maxWindowShifts and tries maxPoolAttempts times to draw a valid post
+// getSFW increments tries maxPollAttempts times to pick a valid post, requesting requestWindowSize posts from the API maxWindowShifts+1 times,
+// and sampling maxRandSamplesPerWindow each time, returning the first SFW post found or an error
 func (r RequestRandomPost) getSFW(subreddit string, sortOrder string) (*reddit.Post, error) {
 	// The Reddit API only seems to return listings of 25 posts max at a time when querying post listings by the "/rising" sort order.
 	// Ordering by "/hot" seems to return listings of 101 or 100.
@@ -128,7 +160,7 @@ func (r RequestRandomPost) getSFW(subreddit string, sortOrder string) (*reddit.P
 		},
 	)
 	if err != nil {
-		return nil, fmt.Errorf("getRandomSFWPost error with http code %q: %q", resp.Status, err.Error())
+		return nil, fmt.Errorf("RequestRandomPost.getSFW error with http code %q: %q", resp.Status, err.Error())
 	}
 
 	// fmt.Print("len posts: ", len(posts))
@@ -136,18 +168,18 @@ func (r RequestRandomPost) getSFW(subreddit string, sortOrder string) (*reddit.P
 	ri := rs.Intn(requestWindowSize)
 	p := posts[ri]
 
-	for i := 0; (goaway.IsProfane(p.Title) || goaway.IsProfane(p.Body)) && i < maxWindowShifts; i++ {
+	for w := 0; w < maxWindowShifts; w++ {
 		// fmt.Println("len posts: ", len(posts))
-
-		// if the content contains profanity, try maxPoolAttempts more times to find a SFW post.
-		for i := 0; i < maxRetriesPerWindow; i++ {
-			ri = rs.Intn(requestWindowSize)
-			if !goaway.IsProfane(posts[ri].Title) && !goaway.IsProfane(posts[ri].Body) {
-				return posts[ri], nil
+		// Try maxPoolAttempts times to find and return a valid SFW post.
+		for i := 0; i < maxRandSamplesPerWindow; i++ {
+			if !goaway.IsProfane(p.Title) && !goaway.IsProfane(p.Body) {
+				return p, nil
 			}
+			ri = rs.Intn(requestWindowSize)
+			p = posts[ri]
 		}
 
-		// advance p to the next maxPoolSize slice of posts
+		// shift posts window to the next slice if no valid post found yet
 		posts, resp, err = reddit.DefaultClient().Subreddit.HotPosts(ctx, subreddit,
 			&reddit.ListOptions{
 				Limit: requestWindowSize,
@@ -156,23 +188,18 @@ func (r RequestRandomPost) getSFW(subreddit string, sortOrder string) (*reddit.P
 		)
 		if err != nil {
 			return nil, errors.Join(
-				fmt.Errorf("getRandomSFWPost error with http code %q: %q", resp.Status, err.Error()),
-				fmt.Errorf("getRandomSFWPost failed to find an SFW post with maxWindowShifts: %d and maxPoolRetries: %d", maxWindowShifts, maxRetriesPerWindow),
+				fmt.Errorf("RequestRandomPost.getSFW error with http code %q: %q", resp.Status, err.Error()),
+				fmt.Errorf("RequestRandomPost.getSFW did not find a valid post:\nrequestWindowSize %d, maxWindowShifts %d, and maxRetriesPerWindow %d",
+					requestWindowSize, maxWindowShifts, maxRandSamplesPerWindow),
 			)
 		}
 		p = posts[ri]
 	}
 	// fmt.Printf("SFW post found with maxWindowShifts: %d and maxPoolRetries: %d\n", maxWindowShifts, maxPoolRetries)
 
-	// Final check for no valid posts found but p was still assigned.
-	// TODO: This error statement shouldn't be so
-	if goaway.IsProfane(p.Title) || goaway.IsProfane(p.Body) {
-		return nil, fmt.Errorf("RequestRandomPost.getSFW was not able to find a valid post with \n, requestWindowSize %d, maxWindowShifts %d, and maxRetriesPerWindow %d",
-			requestWindowSize, maxWindowShifts, maxRetriesPerWindow,
-		)
-	}
-
-	return p, nil
+	// If above loop did not return, no valid post was found within the window polling parameters
+	return nil, fmt.Errorf("RequestRandomPost.getSFW did not find a valid post:\nrequestWindowSize %d, maxWindowShifts %d, and maxRetriesPerWindow %d",
+		requestWindowSize, maxWindowShifts, maxRandSamplesPerWindow)
 }
 
 func (r RequestRandomPost) Request(subreddit string, sortOrder string, censorStrategy string) (*reddit.Post, error) {
@@ -187,6 +214,8 @@ func (r RequestRandomPost) Request(subreddit string, sortOrder string, censorStr
 func requestFromSortOrder(sortOrder string) func(context.Context, string, *reddit.ListOptions) ([]*reddit.Post, *reddit.Response, error) {
 	switch sortOrder {
 	case "hot":
+		// TODO: there's no way with the current reddit package to specify "hot" and a global region
+		// if i want that functionality i'd need to unwrap the wrapper a bit
 		return reddit.DefaultClient().Subreddit.HotPosts
 	case "rising":
 		return reddit.DefaultClient().Subreddit.RisingPosts
--- internal/api/handler_test.go
+++ internal/api/handler_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	goaway "github.com/TwiN/go-away"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -11,8 +12,10 @@ import (
 // These tests do not have fixed results because they query the current reddit API
 // One way to fix this is to use a fixed .json file to emulate query responses
 // I would have to greatly modify the existing code to have that working.
-// In a corporate environment I would probably have to make that change for the sake of scalability,
+// In a corporate environment I would probably have to make that change for the sake of scalability (and to better text my error cases working properly)
 // but for my current needs these tests are fine. Note that they (especially TestGetRandomSFWPost) may not pass 100% of the time for this reason.
+
+// TODO: also test the "maxWindowShift,... etc", parameters. Maybe in a refactored separate config file.
 
 func TestGetNewestPost(t *testing.T) {
 	rs := RequestNewestPost{}
@@ -20,36 +23,38 @@ func TestGetNewestPost(t *testing.T) {
 	p, err := rs.Request("copypasta", "new", "")
 	require.NoError(t, err)
 	assert.NotEmpty(t, p)
-	fmt.Println(p.Title, p.Body)
+	fmt.Println(p.Title, "\n", p.Body)
 }
 
 func TestRequestNewestPost(t *testing.T) {
 	// There is room to do parallel testing here, but
-	rs := RequestRandomPost{}
+	rs := RequestNewestPost{}
 	p1, err := rs.Request("copypasta", "rising", "")
 	require.NoError(t, err)
 	assert.NotEmpty(t, p1.Title)
 	assert.NotEmpty(t, p1.Body)
-	fmt.Println(p1.Title, "\n", p1.Body)
+	fmt.Println("newest from /rising:\n", p1.Title, "\n", p1.Body)
 
 	p2, err := rs.Request("copypasta", "hot", "")
 	require.NoError(t, err)
 	assert.NotEmpty(t, p2.Title)
 	assert.NotEmpty(t, p2.Body)
-	fmt.Println("\n", p2.Title, "\n", p2.Body)
+	fmt.Println("newest from /hot:\n", "\n", p2.Title, "\n", p2.Body)
 
 	p3, err := rs.Request("copypasta", "new", "")
 	require.NoError(t, err)
 	assert.NotEmpty(t, p3.Title)
 	assert.NotEmpty(t, p3.Body)
-	fmt.Println("\n", p3.Title, "\n", p3.Body)
+	fmt.Println("newest from /new:\n", "\n", p3.Title, "\n", p3.Body)
 
 	p4, err := rs.Request("copypasta", "new", "sfw")
 	require.NoError(t, err)
 	assert.NotEmpty(t, p4.Title)
 	assert.NotEmpty(t, p4.Body)
-	assert.NotEqual(t, p3.Body, p4.Body)
-	fmt.Println("\n", p4.Title, "\n", p4.Body)
+	// can't compare with p3 because they are the same if the newest post is sfw
+	assert.NotEqual(t, p2.Body, p4.Body)
+	assert.False(t, goaway.IsProfane(p4.Title) || goaway.IsProfane(p4.Body))
+	fmt.Println("newest sfw from /new:\n", "\n", p4.Title, "\n", p4.Body)
 
 	// TODO: I need to put extra words in the profanity filter lmao
 
